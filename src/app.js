33c763bba5b2b9ce03aae072d841171e9ef6d7ff
fath-nasrudin <fath.nasrudin@gmail.com>
2022-11-05 11:48:47 +0700

    add swagger-ui and documentation (draft)

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+// const swaggerUi = require('swagger-ui-express');
+// const apiDoc = require('../docs');
 
 const routes = require('./routes');
 const { api } = require('./config');
@@ -19,6 +21,9 @@ app.use(express.urlencoded({ extended: true }));
 
 // root
 app.get('/', (req, res, next) => { res.json({ message: 'root' }); });
+
+// documentation
+// app.use(`${api.path}/documentation`, swaggerUi.serve, swaggerUi.setup(apiDoc));
 
 // routes for api
 app.use(api.path, routes);
